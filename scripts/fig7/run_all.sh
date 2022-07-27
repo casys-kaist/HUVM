675bddc7c256c86fb3d13087ab1ab1e1ae8b916c
Ubuntu <ubuntu@ip-172-31-75-157.ec2.internal>
2022-07-27 08:51:54 +0000

    script fix

--- scripts/fig7/run_all.sh
+++ scripts/fig7/run_all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-slack "fig7 start"
 ./case1_pagerank.sh
-slack "case 1 fin"
 ./case2_bfs.sh
-slack "case 2 fin"
 ./case3_wcc.sh
-slack "case 3 fin"
 ./case4_louvain.sh
-slack "case 4 fin"
